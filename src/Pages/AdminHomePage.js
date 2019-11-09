48d29f8894a349e3d03171b34ca133244c58e688
jainhere <njain_be16@thapar.edu>
2019-11-10 02:03:01 +0530

    UI improvement

--- src/Pages/AdminHomePage.js
+++ src/Pages/AdminHomePage.js
@@ -60,49 +60,57 @@ class  AdminHomePage extends Component {
         
         return (
             <div className="App">
+
+                <div class="row h-100 justify-content-center m-3 align-items-center">
+
+                    <div class="col-sm-8">
+                        Click on the School name of the school which you want to explore.
+                        <div style={{background:'white'}}>
+                            <SchoolTableComponent data={this.state.schoolsPresent} headers={["School ID","School Name", "School Address"]} />
+                        </div>
+                    </div>
                 
-                <div style={{background:'white'}}>
-                    <SchoolTableComponent data={this.state.schoolsPresent} headers={["School ID","School Name", "School Address"]} />
+                    <div class="col-sm-4">
+                        Add a new school.
+                        <InputGroup className="mb-3" >
+                            <InputGroup.Prepend>
+                            <InputGroup.Text 
+                                id="inputGroup-sizing-default"
+                                style={{width:100}}
+                            >Name</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl 
+                                placeholder="Enter School name"
+                                aria-label="Default"
+                                aria-describedby="inputGroup-sizing-default"
+                                onChange={this.handleChange.bind(this)}
+                                value={this.state.newschool.SchoolName}
+                                name='SchoolName'
+                            />
+                        </InputGroup>
+
+                        <InputGroup className="mb-3" >
+                            <InputGroup.Prepend>
+                            <InputGroup.Text 
+                                id="inputGroup-sizing-default"
+                                style={{width:100}}
+                            >Address</InputGroup.Text>
+                            </InputGroup.Prepend>
+                            <FormControl
+                                placeholder="Enter School address"
+                                aria-label="Default"
+                                aria-describedby="inputGroup-sizing-default"
+                                onChange={this.handleChange.bind(this)}
+                                value={this.state.newschool.SchoolAddress}
+                                name='SchoolAddress'
+                            />
+                        </InputGroup>
+
+                        <Button variant="outline-danger" onClick={this.createschool.bind(this)}>
+                            Create School
+                        </Button>                
+                    </div>
                 </div>
-                
-                <br />
-                <InputGroup className="mb-3" style = {{width:"50%"}}>
-                    <InputGroup.Prepend>
-                    <InputGroup.Text 
-                        id="inputGroup-sizing-default"
-                        style={{width:100}}
-                    >Name</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="Enter School name"
-                        aria-label="Default"
-                        aria-describedby="inputGroup-sizing-default"
-                        onChange={this.handleChange.bind(this)}
-                        value={this.state.newschool.SchoolName}
-                        name='SchoolName'
-                    />
-                </InputGroup>
-
-                <InputGroup className="mb-3" style = {{width:"50%"}}>
-                    <InputGroup.Prepend>
-                    <InputGroup.Text 
-                        id="inputGroup-sizing-default"
-                        style={{width:100}}
-                    >Address</InputGroup.Text>
-                    </InputGroup.Prepend>
-                    <FormControl
-                        placeholder="Enter School address"
-                        aria-label="Default"
-                        aria-describedby="inputGroup-sizing-default"
-                        onChange={this.handleChange.bind(this)}
-                        value={this.state.newschool.SchoolAddress}
-                        name='SchoolAddress'
-                    />
-                </InputGroup>
-
-                <Button variant="outline-danger" onClick={this.createschool.bind(this)}>
-                    Create School
-                </Button>                
             </div>
         );
     }
--- src/Pages/CreateClassroom.js
+++ src/Pages/CreateClassroom.js
@@ -90,68 +90,81 @@ class CreateSchool extends React.Component {
     return (
         <div className="App">
           
+          <div class="row h-100 justify-content-center m-3 align-items-center">
+            <div class="col-sm-8"> 
+              Classes present in School {SchoolId} are: 
+              <p style={{fontSize:20}}><em>Click on the Class Id to explore a class.</em></p>
+              <br/>
+              <div style={{background:'white'}}>
+                        <ClassTableComponent data={this.state.ClassesPresent} headers={["Class Id","School Id","ClassName","isActive"]} SchoolId ={SchoolId}/>
+              </div>
+            </div>
+
+            <div class="col-sm-4">
+
+          
+              Create a Classroom
+              <br />
+              <br />
+              <InputGroup className="mb-3" >
+                  <InputGroup.Prepend>
+                  <InputGroup.Text id="inputGroup-sizing-default" style={{width:140}}>Class Name</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                  placeholder = "Enter the class name"
+                  aria-label="Default"
+                  aria-describedby="inputGroup-sizing-default"
+                  onChange={this.handleChange.bind(this)}
+                  value={this.state.NewClass.className}
+                  name='className'
+                  />
+              </InputGroup>
+
+              <InputGroup className="mb-3" >
+                  <InputGroup.Prepend>
+                  <InputGroup.Text id="inputGroup-sizing-default" style={{width:140}}>Active or IsActive</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl
+                  placeholder = "True or False"
+                  aria-label="Default"
+                  aria-describedby="inputGroup-sizing-default"
+                  onChange={this.handleChange.bind(this)}
+                  value={this.state.NewClass.isActive}
+                  name='isActive'
+                  />
+              </InputGroup>
+
+              
+              
+                <Button variant="outline-danger" onClick={this.createClass.bind(this)}>
+                    Create Classroom
+                </Button>
+
+              <br />
+              <br />
+              Delete a Classroom
+              <br />
+              <br />
+
+                <InputGroup className="mb-3" >
+                  <InputGroup.Prepend>
+                  <InputGroup.Text id="inputGroup-sizing-default" >Class Id</InputGroup.Text>
+                  </InputGroup.Prepend>
+                  <FormControl 
+                  placeholder = "Enter the class ID of the class you want to delete"
+                  aria-label="Default"
+                  aria-describedby="inputGroup-sizing-default"
+                  onChange={this.handleDeleteChange.bind(this)}
+                  value={this.state.deleteclassid}
+                  />
+              </InputGroup>
+
+              <Button variant="outline-danger" onClick={this.deleteClass.bind(this)}>
+                    Delete Classroom
+                </Button>
             
-          <div style={{background:'white'}}>
-                    <ClassTableComponent data={this.state.ClassesPresent} headers={["Class Id","School Id","ClassName","isActive"]} SchoolId ={SchoolId}/>
+            </div>
           </div>
-
-          <br />
-
-          
-
-            <InputGroup className="mb-3" style = {{width:"50%"}}>
-                <InputGroup.Prepend>
-                <InputGroup.Text id="inputGroup-sizing-default" style={{width:140}}>Class Name</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                aria-label="Default"
-                aria-describedby="inputGroup-sizing-default"
-                onChange={this.handleChange.bind(this)}
-                value={this.state.NewClass.className}
-                name='className'
-                />
-            </InputGroup>
-
-            <InputGroup className="mb-3" style = {{width:"50%"}}>
-                <InputGroup.Prepend>
-                <InputGroup.Text id="inputGroup-sizing-default" style={{width:140}}>Active or IsActive</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl
-                aria-label="Default"
-                aria-describedby="inputGroup-sizing-default"
-                onChange={this.handleChange.bind(this)}
-                value={this.state.NewClass.isActive}
-                name='isActive'
-                />
-            </InputGroup>
-
-            
-            
-              <Button variant="outline-danger" onClick={this.createClass.bind(this)}>
-                  Create Classroom
-              </Button>
-
-            <br />
-
-              <InputGroup className="mb-3" style = {{width:"70%"}}>
-                <InputGroup.Prepend>
-                <InputGroup.Text id="inputGroup-sizing-default" style={{width:80}}>Class Id</InputGroup.Text>
-                </InputGroup.Prepend>
-                <FormControl 
-                placeholder = "Enter the class ID of the class you want to delete"
-                aria-label="Default"
-                aria-describedby="inputGroup-sizing-default"
-                onChange={this.handleDeleteChange.bind(this)}
-                value={this.state.deleteclassid}
-                />
-            </InputGroup>
-
-            <Button variant="outline-danger" onClick={this.deleteClass.bind(this)}>
-                  Delete Classroom
-              </Button>
-            
-            
-          
         </div>
       );
   }
